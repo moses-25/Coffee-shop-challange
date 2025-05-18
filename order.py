144c133812cb05fed4645c78fa0b3f7ff7d826bd
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-05-18 16:49:00 +0300

    refactor: remove unused name property and its setter from Order class

--- order.py
+++ order.py
@@ -27,17 +27,6 @@ class Order:
         return self._price
 
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 15:
-            self._name = value
-        else:
-            raise ValueError("Name must be a string between 1 and 15 characters.")
-
     def orders(self):
         return [order for order in Order.all if order.customer == self]
 
