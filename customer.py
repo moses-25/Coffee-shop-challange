1d4fc9d6a10c14c8857eabf3b6cf1aeb18733db2
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-05-18 16:19:33 +0300

    fix: uncomment name setter method in Customer class for proper name validation

--- customer.py
+++ customer.py
@@ -6,8 +6,8 @@ class Customer:
     def name(self):
         return self._name
 
-   # @name.setter
-   # def name(self, value):
+    @name.setter
+    def name(self, value):
         if isinstance(value, str) and 1 <= len(value) <= 15:
             self._name = value
         else:
